0926ede2d4cf33b8cff4735b2c740dc510bf95ef
evolvingkid1 <evolvingkid1@github.com>
2020-12-16 17:12:23 -0800

    repo init

---
+++ src/main/java/com/evolvingkid1/workoutapi/WorkoutapiApplication.java
@@ -0,0 +1,13 @@
+package com.evolvingkid1.workoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutapiApplication.class, args);
+	}
+
+}
